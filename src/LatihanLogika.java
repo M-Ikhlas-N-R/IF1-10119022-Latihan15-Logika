2ee2f20c0c9774e5ffed25ea3762563a7a38a0f7
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-10-16 09:28:23 +0700

    Add files via upload

---
+++ src/LatihanLogika.java
@@ -0,0 +1,20 @@
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Latihan Logika
+ *
+ */
+public class LatihanLogika {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+
+    }
+}
